b81e8fda5d4235291ae83863f8502a1f671695da
Lukman <113237852+03lukman@users.noreply.github.com>
2025-07-31 02:49:24 +0700

    Update camera.py

--- src/camera.py
+++ src/camera.py
@@ -2,7 +2,7 @@ import time
 import threading
 
 class Camera(threading.Thread):
-    def __init__(self, cap_getter, frame_setter, error_callback, fps=30):
+    def __init__(self, cap_getter, frame_setter, error_callback, fps=20):
         super().__init__(daemon=True)
         self.cap_getter = cap_getter
         self.frame_setter = frame_setter
@@ -13,25 +13,28 @@ class Camera(threading.Thread):
 
         self.cap = self.cap_getter()
         self.frame_count = 0
-        self.start_time = time.time()
+        self.start_time = time.perf_counter()
         self.current_fps = 0
 
     def run(self):
         while self.running:
             start_time = time.time()
+
             ret, frame = self.cap.read()
             if not ret:
                 print("[ERROR] Gagal membaca frame")
                 self.error_callback()
-                self.running = False
-                return
+                self.running = False  # stop thread
+                return  # keluar dari run
 
             self.frame_setter(frame)
             time.sleep(self.frame_delay)
 
+            # Update penghitung frame
             self.frame_count += 1
             elapsed_total = time.time() - self.start_time
 
+            # Update FPS setiap 1 detik
             if elapsed_total >= 1.0:
                 self.current_fps = self.frame_count / elapsed_total
                 self.frame_count = 0
